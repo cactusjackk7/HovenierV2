52b573d478cfcd590eb02f91b35983abef3a8aa7
cactusjackk7 <cactusjackk7@gmail.com>
2022-11-22 14:55:37 +0100

    examenopdracht

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Webshop Hovenier</title>
+    <link rel="stylesheet" href="assets/style.css" type="text/css"/>
+</head>
+<body>
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="shop.php">Shop</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="welcome.php">Profiel</a></li>
+</ul>
+</body>
+</html>
